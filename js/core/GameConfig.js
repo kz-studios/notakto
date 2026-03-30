c06f191a3416d6ef26bd31062436e87540f5e277
KaizenCyrus <kz.studios.freelance@gmail.com>
2026-03-30 19:57:29 +0800

    Implement game setup data binding and UI state
    - Bind GameConfig property values dynamically to DOM elements by iterating over .controls datasets during initialization and user interaction.
    - Setting adjustment arrows are now visually disabled when a minimum or maximum limit is reached.
    - Selecting the "No time limit" option now successfully grays out the related time adjustment arrows and displays an infinity symbol.
    - Added #isThereNoGameTimeLimit property in GameConfig.
    - The settings layout is perfectly aligned across all rows regardless of the text length inside the options.

--- js/core/GameConfig.js
+++ js/core/GameConfig.js
@@ -1,21 +1,76 @@
 class GameConfig {
-    #numofPlayers
+    #numOfPlayers
     #numOfGrids
     #numOfRounds
     #playerTurnTimeLimit
+    #isThereNoGameTimeLimit
     #gameTimeLimit
     #gameMode
 
     constructor() {
         this.#numOfGrids = 3;
+        this.#numOfPlayers = 2;
+        this.#numOfRounds = 5;
+        this.#playerTurnTimeLimit = 5;
+        this.#gameTimeLimit = 60;
+        this.#isThereNoGameTimeLimit = false;
+        this.#gameMode = 'Normal';
     }
 
     get numOfGrids() {
         return this.#numOfGrids;
     }
 
-    set numOfGrids(numOfGrids) {
-        this.#numOfGrids = numOfGrids;
+    set numOfGrids(value) {
+        this.#numOfGrids = value;
+    }
+
+    get numOfPlayers() {
+        return this.#numOfPlayers;
+    }
+
+    set numOfPlayers(value) {
+        this.#numOfPlayers = value;
+    }
+
+    get numOfRounds() {
+        return this.#numOfRounds;
+    }
+
+    set numOfRounds(value) {
+        this.#numOfRounds = value;
+    }
+
+    get playerTurnTimeLimit() {
+        return this.#playerTurnTimeLimit;
+    }
+
+    set playerTurnTimeLimit(value) {
+        this.#playerTurnTimeLimit = value;
+    }
+
+    get gameTimeLimit() {
+        return this.#gameTimeLimit;
+    }
+
+    set gameTimeLimit(value) {
+        this.#gameTimeLimit = value;
+    }
+
+    get isThereNoGameTimeLimit() {
+        return this.#isThereNoGameTimeLimit;
+    }
+
+    set isThereNoGameTimeLimit(value) {
+        this.#isThereNoGameTimeLimit = value;
+    }
+
+    get gameMode() {
+        return this.#gameMode;
+    }
+
+    set gameMode(value) {
+        this.#gameMode = value;
     }
 }
 
--- js/ui/screens/GameSetupScreen.js
+++ js/ui/screens/GameSetupScreen.js
@@ -5,9 +5,14 @@ export default class GameSetupScreen {
         this.screen = document.querySelector('#screen-game-setup');
         this.settingsForm = this.screen.querySelector('#screen-game-setup-settings-container');
         this.gameSettingsContainer = this.settingsForm.querySelector('#game-settings-container');
+        this.controlElements = this.settingsForm.querySelectorAll('.game-settings-option .controls');
+        this.chkNoTimeLimit = this.settingsForm.querySelector('#checkbox-game-time-limit');
+
         this.btnBack = this.screen.querySelector('.btn-back');
         this.btnHowToPlay = this.screen.querySelector('.btn-how-to-play');
         this.btnStart = this.screen.querySelector('.btn-start');
+
+        this.renderGameSetupValues();
 
         this.gameSettingsContainer.addEventListener('click', (event) => {
             const btn = event.target;
@@ -31,7 +36,71 @@ export default class GameSetupScreen {
             } else return;
 
             gameConfig[settingKey] = newValue;
-            displaySpan.textContent = gameConfig[settingKey];
+
+            this.updateArrowState(controlsContainer, min, max, newValue);
+
+            let displayText = gameConfig[settingKey];
+            if ('unit' in controlsContainer.dataset) {
+                displayText += controlsContainer.dataset.unit;
+            }
+            displaySpan.textContent = displayText;
         })
+
+        this.chkNoTimeLimit.addEventListener('change', (event) => {
+            const gameTimeLimitStepperGroup = this.gameSettingsContainer.querySelector('.controls[data-setting="gameTimeLimit"] .stepper-group');
+            const gameTimeLimitDisplayValue = gameTimeLimitStepperGroup.querySelector('span');
+
+            if (event.target.checked) {
+                gameConfig.isThereNoGameTimeLimit = true;
+                gameTimeLimitStepperGroup.classList.toggle('disabled', true);
+                gameTimeLimitDisplayValue.textContent = '∞';
+            } else {
+                gameConfig.isThereNoGameTimeLimit = false;
+                gameTimeLimitStepperGroup.classList.toggle('disabled', false);
+                gameTimeLimitDisplayValue.textContent = gameConfig.gameTimeLimit + 'sec';
+            }
+
+            this.chkNoTimeLimit.checked = gameConfig.isThereNoGameTimeLimit;
+        })
+    }
+
+    renderGameSetupValues() {
+        for (const controlElement of this.controlElements) {
+            const settingKey = controlElement.dataset.setting;
+            const displaySpan = controlElement.querySelector('span');
+
+            if (controlElement.dataset.setting !== 'gameMode') {
+                const min = parseInt(controlElement.dataset.min);
+                const max = parseInt(controlElement.dataset.max);
+
+                this.updateArrowState(controlElement, min, max, gameConfig[settingKey]);
+
+                if ('unit' in controlElement.dataset) displaySpan.textContent = controlElement.dataset.unit;
+                displaySpan.textContent = gameConfig[settingKey] + displaySpan.textContent;
+            }
+            else if (controlElement.dataset.setting == 'gameMode') {
+                const radioGameModeNormal = controlElement.querySelector(`input[type="radio"][value="${gameConfig.gameMode}"]`);
+                radioGameModeNormal.checked = true;
+            }
+        }
+
+        this.chkNoTimeLimit.checked = gameConfig.isThereNoGameTimeLimit;
+    }
+
+    updateArrowState(controlElement, min, max, value) {
+        const btnDecrease = controlElement.querySelector('button.btn-decrease');
+        const btnIncrease = controlElement.querySelector('button.btn-increase');
+        
+        if (value === min) {
+            btnDecrease.classList.toggle('disabled', true);
+        } else {
+            btnDecrease.classList.toggle('disabled', false);
+        }
+
+        if (value === max) {
+            btnIncrease.classList.toggle('disabled', true);
+        } else {
+            btnIncrease.classList.toggle('disabled', false);
+        }
     }
 }
